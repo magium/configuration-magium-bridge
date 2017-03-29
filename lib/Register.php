1e51ad4eac9d30123f2d01e4129e212bfa7f2dcf
Kevin Schroeder <kschroeder@mirageworks.com>
2017-03-29 08:42:01 -0500

    Removed an unused function

--- lib/Register.php
+++ lib/Register.php
@@ -29,49 +29,4 @@ class Register
         );
     }
 
-    public function register1(AbstractTestCase $testCase, MagiumConfigurationFactoryInterface $factory)
-    {
-        $initializerDi = Initializer::getInitializationDependencyInjectionContainer();
-        $instanceManager = $initializerDi->instanceManager();
-        if ($instanceManager->hasSharedInstance(MagiumConfigurationFactoryInterface::class)) {
-            return;
-        }
-
-        $manager = $factory->getManager();
-        $builder = $factory->getBuilder();
-
-        $testCase->setTypePreference(
-            ConfigurationProviderInterface::class,
-            ConfigurationProvider::class
-        );
-
-        $instanceManager->addSharedInstance($factory, MagiumConfigurationFactoryInterface::class);
-        $instanceManager->addSharedInstance($manager, ManagerInterface::class);
-        $instanceManager->addSharedInstance($builder, BuilderInterface::class);
-
-        $instanceManager->unsetTypePreferences(ConfigurationProviderInterface::class);
-        $instanceManager->setTypePreference(
-            ConfigurationProviderInterface::class,
-            [ConfigurationProvider::class]
-        );
-        $initializer = $initializerDi->get(Initializer::class);
-        /** @var $initializer Initializer */
-        $initializer->initialize($testCase);
-
-        $di = $testCase->getDi();
-        $di->instanceManager()->addSharedInstance($builder, BuilderInterface::class);
-        $di->instanceManager()->addSharedInstance($initializerDi->get(ManagerInterface::class), ManagerInterface::class);
-        $testCase->setTypePreference(
-            MagiumConfigurationFactoryInterface::class,
-            get_class($factory)
-        );
-        $di->instanceManager()->addSharedInstance($factory, get_class($factory));
-
-        $provider = $testCase->get(ConfigurationProvider::class);
-
-        if ($provider instanceof ConfigurationProvider) {
-            $provider->configureDi($di);
-        }
-    }
-
 }
